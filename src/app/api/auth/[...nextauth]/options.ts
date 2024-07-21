de6a536444f88df600305b077291313ecd317c7b
Umair <umairwaraich2019@gmail.com>
2024-07-21 14:31:47 +0500

    add next-auth setup for my project

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,84 @@
+import { connectDB } from "@/lib/connectDB";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import UserModel from "@/models/user.model";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any): Promise<any> {
+        await connectDB();
+
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier.email },
+              { username: credentials.identifier.username },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("User not found with this email");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before login.");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isPasswordCorrect) {
+            throw new Error("Incorrect Password!");
+          }
+
+          return user;
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+        token.email = user.email;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+};
